812b1fbe6893a27512b5bf421d877979c791e1cb
difadlyaulhaq <difadlyaulhaq2@gmail.com>
2025-05-16 14:10:59 +0700

    refactor: Extract splash screen content into separate widgets for better readability and maintainability

--- lib/pages/splashscreen_page.dart
+++ lib/pages/splashscreen_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-
 import '../theme/theme.dart';
 
 class Splashscreen extends StatefulWidget {
@@ -16,38 +15,71 @@ class _SplashscreenState extends State<Splashscreen> {
     super.initState();
     Future.delayed(const Duration(seconds: 3), () {
       context.go('/select-role');
-
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return  Scaffold(
       backgroundColor: whiteColor,
       body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              "assets/logo.png",
-              width: 120,
-              height: 120,
-            ),
-            const SizedBox(height: 20),
-            Text(
-              "Al Farooq Center:",
-              style: blackColorTextStyle.copyWith(fontSize: 16),
-              textAlign: TextAlign.center,
-            ),
-            Text(
-              "Omar Bin Al Khattab School",
-              style: blackColorTextStyle.copyWith(fontSize: 16),
-              textAlign: TextAlign.center,
-            ),
-          ],
-        ),
+        child: SplashContent(),
       ),
+    );
+  }
+}
+
+/// Custom widget untuk konten splash screen
+class SplashContent extends StatelessWidget {
+  const SplashContent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: const [
+        LogoImage(),
+        SizedBox(height: 20),
+        AppTitleText(),
+      ],
+    );
+  }
+}
+
+/// Custom widget untuk logo
+class LogoImage extends StatelessWidget {
+  const LogoImage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(
+      "assets/logo.png",
+      width: 120,
+      height: 120,
+    );
+  }
+}
+
+/// Custom widget untuk teks judul aplikasi
+class AppTitleText extends StatelessWidget {
+  const AppTitleText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          "Al Farooq Center:",
+          style: blackColorTextStyle.copyWith(fontSize: 16),
+          textAlign: TextAlign.center,
+        ),
+        Text(
+          "Omar Bin Al Khattab School",
+          style: blackColorTextStyle.copyWith(fontSize: 16),
+          textAlign: TextAlign.center,
+        ),
+      ],
     );
   }
 }
